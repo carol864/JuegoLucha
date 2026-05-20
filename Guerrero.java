48b29462fd75db2ebf7d3f8353c05a990fcc6b8a
Carol Castro <castrokarol214@gmail.com>
2026-05-19 21:29:37 -0500

    Clase Guerrero

---
+++ Guerrero.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Guerrero extends Personaje{
+
+    public Guerrero(String nombre){
+        super(nombre);
+    }
+
+    public void atacar(Personaje enemigo){
+        Random r = new Random();
+
+        int danio = r.nextInt(21)+10;
+
+        enemigo.recibirDano(danio);
+
+        System.out.println( nombre + "ataco a " + enemigo.getNombre() + " causando" + danio);
+    }
+}
