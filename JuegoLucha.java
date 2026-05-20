2d8b2c5f029288a81c8ab777bc1f2eb826e17f9d
Carol Castro <castrokarol214@gmail.com>
2026-05-20 13:04:44 -0500

    Clase JugadorLucha

--- JuegoLucha.java
+++ JuegoLucha.java
@@ -3,38 +3,41 @@ class JuegoLucha {
     private Personaje jugador2 ;
 
     // Constructor para inicializar los personajes
-    public JuegoLucha ( String nombre1 , String nombre2 ) {
-        this.jugador1 = new Personaje ( nombre1 );
-        this.jugador2 = new Personaje ( nombre2 );
+    public JuegoLucha ( Personaje jugador1, Personaje jugador2 ) {
+
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
     }
+
     // Metodo para iniciar la pelea
     public void iniciarPelea () {
 
-        System.out.println ("La pelea comienza entre " +
-            jugador1.getNombre() + " y " + jugador2.getNombre() + "...");
+        while(jugador1.estaVivo() && jugador2.estaVivo()){
 
-        while ( jugador1.estaVivo() && jugador2.estaVivo()) {
-            turno ( jugador1 , jugador2 );
-            if ( jugador2.estaVivo()) {
-                turno ( jugador2 , jugador1 );
-        }
-    }
+            turno(jugador1, jugador2);
 
-        // Mostrar el resultado de la pelea
-        if ( jugador1.estaVivo ()) {
-                System.out.println ( jugador1 . getNombre () + " ha ganado la pelea .");
-            } else {
-                System.out.println ( jugador2 . getNombre () + " ha ganado la pelea .");
+            if(jugador2.estaVivo()){
+                turno(jugador2, jugador1);
+            }
         }
+
+        if(jugador1.estaVivo()){
+            System.out.println(jugador1.getNombre() + " ganó");
+        }
+
+        else{
+            System.out.println(jugador2.getNombre() + " ganó");
+        }
+
     }
+    
 
     // Metodo que representa un turno de ataque
     private void turno ( Personaje atacante , Personaje defensor ) {
-        System.out.println (" Turno de " + atacante.getNombre() + ". Puntos de vida de " + defensor.getNombre() + ": " + defensor.getPuntosDeVida ());
         
-        atacante.atacar ( defensor ); 
+        atacante.atacar(defensor);
 
-        System.out.println ( defensor.getNombre () + " ahora tiene " + defensor.getPuntosDeVida () + " puntos de vida .");
+        System.out.println(defensor.getNombre() + " tiene " + defensor.getPuntosDeVida() + " HP");
     }
 
 }
