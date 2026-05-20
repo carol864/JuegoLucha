dbd65141d17ee02ff3467d789bfd0430e343ce54
Carol Castro <castrokarol214@gmail.com>
2026-05-19 21:00:49 -0500

    Clase abstracta Personaje

--- Personaje.java
+++ Personaje.java
@@ -1,10 +1,8 @@
-import java.util.Random;
 
-class Personaje {
-    private String nombre;
-    private int puntosDeVida;
-    private final int MAX_DANO = 30;
-    private final int MIN_DANO = 10;
+public abstract class Personaje {
+
+    protected String nombre;
+    protected int puntosDeVida;
 
     //Constructor para inicializar los datos del personaje
     public Personaje(String nombre) {
@@ -12,17 +10,13 @@ class Personaje {
         this.puntosDeVida = 100; //Todos comienzan con 100 puntos de vida
     }
 
-    //Metodo para realizar un ataque a otro personaje
-    public void atacar(Personaje oponente) {
-        Random rand = new Random();
-        int dano = rand.nextInt(MAX_DANO - MIN_DANO + 1) + MIN_DANO; //Genera un daño aleatorio entre 10 y 30
-        oponente.recibirDano(dano);
-        System.out.println(this.nombre + " ataca a " + oponente.getNombre() + " causando " + dano + " puntos de daño.");
-    }
+    public abstract void atacar(Personaje enemigo);
 
     //Metodo para recibir daño
     public void recibirDano(int dano) {
+
         this.puntosDeVida -= dano;
+        
         if (this.puntosDeVida < 0) {
             this.puntosDeVida = 0; //No se puede tenermenos de 0 puntos de vida
         }
